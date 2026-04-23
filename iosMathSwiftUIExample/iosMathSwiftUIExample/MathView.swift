e9aad349f37a5e523938c252d6916a5238f0a98c
Madiyar Aitbayev <maitbayev@snapchat.com>
2026-04-23 17:48:08 +0100

    Add iosMathSwiftUIExample

--- iosMathSwiftUIExample/iosMathSwiftUIExample/MathView.swift
+++ iosMathSwiftUIExample/iosMathSwiftUIExample/MathView.swift
@@ -6,77 +6,92 @@
 //
 
 import SwiftUI
-import UIKit
 import iosMath
 
 enum MathFontFace: String, CaseIterable, Identifiable {
-    case latinModern = "Latin Modern Math"
-    case termes = "TeX Gyre Termes"
-    case xits = "XITS Math"
+  case latinModern = "Latin Modern Math"
+  case termes = "TeX Gyre Termes"
+  case xits = "XITS Math"
 
-    var id: String { rawValue }
+  var id: String { rawValue }
 
-    func font(size: CGFloat) -> MTFont {
-        let manager = MTFontManager.sharedInstance()
-        switch self {
-        case .latinModern: return manager.latinModernFont(withSize: size)
-        case .termes:      return manager.termesFont(withSize: size)
-        case .xits:        return manager.xitsFont(withSize: size)
-        }
+  func font(size: CGFloat) -> MTFont {
+    let manager = MTFontManager.sharedInstance()
+    switch self {
+    case .latinModern: return manager.latinModernFont(withSize: size)
+    case .termes: return manager.termesFont(withSize: size)
+    case .xits: return manager.xitsFont(withSize: size)
     }
+  }
 }
 
 enum MathAlignment: CaseIterable {
-    case left, center, right
+  case left, center, right
 
-    var mtAlignment: MTTextAlignment {
-        switch self {
-        case .left:   return .left
-        case .center: return .center
-        case .right:  return .right
-        }
+  var mtAlignment: MTTextAlignment {
+    switch self {
+    case .left: return .left
+    case .center: return .center
+    case .right: return .right
     }
+  }
 }
 
 enum MathMode: CaseIterable {
-    case display, text
+  case display, text
 
-    var mtMode: MTMathUILabelMode {
-        switch self {
-        case .display: return .display
-        case .text:    return .text
-        }
+  var mtMode: MTMathUILabelMode {
+    switch self {
+    case .display: return .display
+    case .text: return .text
     }
+  }
 }
 
-struct MathView: UIViewRepresentable {
-    var latex: String
-    var fontFace: MathFontFace = .latinModern
-    var fontSize: CGFloat = 20
-    var textColor: UIColor = .label
-    var backgroundColor: UIColor = .clear
-    var alignment: MathAlignment = .left
-    var mode: MathMode = .display
-    var contentInsets: UIEdgeInsets = .zero
+struct MathView {
+  var latex: String
+  var fontFace: MathFontFace = .latinModern
+  var fontSize: CGFloat = 20
+  var textColor: UXColor = .label
+  var backgroundColor: UXColor = .clear
+  var alignment: MathAlignment = .left
+  var mode: MathMode = .display
+  var contentInsets: MTEdgeInsets = .zero
 
+  fileprivate func apply(to label: MTMathUILabel) {
+    label.latex = latex
+    label.font = fontFace.font(size: fontSize)
+    label.fontSize = fontSize
+    label.textColor = textColor
+    label.backgroundColor = backgroundColor
+    label.textAlignment = alignment.mtAlignment
+    label.labelMode = mode.mtMode
+    label.contentInsets = contentInsets
+  }
+}
+
+#if os(iOS)
+  extension MathView: UIViewRepresentable {
     func makeUIView(context: Context) -> MTMathUILabel {
-        let label = MTMathUILabel()
-        apply(to: label)
-        return label
+      let label = MTMathUILabel()
+      apply(to: label)
+      return label
     }
 
     func updateUIView(_ uiView: MTMathUILabel, context: Context) {
-        apply(to: uiView)
+      apply(to: uiView)
+    }
+  }
+#elseif os(macOS)
+  extension MathView: NSViewRepresentable {
+    func makeNSView(context: Context) -> MTMathUILabel {
+      let label = MTMathUILabel()
+      apply(to: label)
+      return label
     }
 
-    private func apply(to label: MTMathUILabel) {
-        label.latex = latex
-        label.font = fontFace.font(size: fontSize)
-        label.fontSize = fontSize
-        label.textColor = textColor
-        label.backgroundColor = backgroundColor
-        label.textAlignment = alignment.mtAlignment
-        label.labelMode = mode.mtMode
-        label.contentInsets = contentInsets
+    func updateNSView(_ nsView: MTMathUILabel, context: Context) {
+      apply(to: nsView)
     }
-}
+  }
+#endif
